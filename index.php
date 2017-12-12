481b915ae457c401e304db13d690e65eee71fbaa
Sarah Harvey <desertriverjem@gmail.com>
2017-12-11 20:51:42 -0700

    updated to allow user to see charts they have created

--- index.php
+++ index.php
@@ -14,6 +14,42 @@
             }
         }
         
+        function body(){
+            global $user;
+            parent::body();
+            if($user->authenticated()){
+                $this->add($this->displayCharts());
+            }
+        }
+        
+        function displayCharts(){
+            global $db;
+            $uid=$_SESSION["user"]["id"];
+            $sql="SELECT chartid, chartname, rownum, colnum FROM charts WHERE uid = :uid";
+            $prepared=$db->prepare($sql, array($db->ATTR_CURSOR => $db->CURSOR_FWDONLY));
+            $prepared->execute(array(':uid'=> $uid));
+            $results = $prepared->fetchAll(PDO::FETCH_ASSOC);
+           
+            
+            
+            if(count($results )> 0){
+                echo "<p>Your tables:</p>\n";
+                echo "<table  class=\"table table-bordered\"  style=\"width:75%\">\n";
+                echo "<tbody>\n";
+                echo "<tr>\n";
+                echo "<th>ID</th>\n";
+                echo "<th>Chart Name</th>\n";
+                echo"<th>Number of rows</th>\n";
+                echo"<th>Number of columns</th>\n";
+                foreach($results as $row){
+                    echo "<tr>\n<td>" . $row['chartid']. "</td>\n<td>" . $row['chartname'] . "</td>\n<td>" . $row['rownum'] . "</td>\n<td>" . $row['colnum'] . "</td>\n</tr>\n";
+                    
+                }
+                echo "</tbody>\n";
+                echo "</table>\n";
+            }
+            
+        }
     }
     $index = new Index();
     $index->generate();
